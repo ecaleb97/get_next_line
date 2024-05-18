d526ae3c77eccf74bc7705360af8ba89f6563a6c
ecaleb97 <valley_rob.0c@icloud.com>
2024-05-18 20:06:52 +0200

    add some update to header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 19:42:43 by envillan          #+#    #+#             */
-/*   Updated: 2024/05/18 19:47:04 by envillan         ###   ########.fr       */
+/*   Updated: 2024/05/18 20:06:03 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,11 @@
 
 # endif
 
+void    *ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *dest, size_t n);
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(char *str, int c);
-size_t	ft_strlen(char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(const char *s, int i);
+size_t	ft_strlen(const char *s);
 
 #endif
