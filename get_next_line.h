c4089d10cce4efd65b984cf9c0ed754da534877e
ecaleb97 <valle_rob.0c@icloud.com>
2024-05-24 10:20:35 +0200

    add new function and remove unnecesary in header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,27 +6,26 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 19:42:43 by envillan          #+#    #+#             */
-/*   Updated: 2024/05/18 20:06:03 by envillan         ###   ########.fr       */
+/*   Updated: 2024/05/24 09:49:10 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stddef.h>
 
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
 
-# endif
-
-void    *ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *dest, size_t n);
 char	*get_next_line(int fd);
+char	*ft_strdup(char *s);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strchr(const char *s, int i);
 size_t	ft_strlen(const char *s);
+void	fill_str(char *res, char *s1, char *s2);
 
 #endif
