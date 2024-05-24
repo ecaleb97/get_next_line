2a17dd8de0cfc3eac80b6b25c155f6553582ee3c
ecaleb97 <valle_rob.0c@icloud.com>
2024-05-24 10:48:29 +0200

    fix error in header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 19:42:43 by envillan          #+#    #+#             */
-/*   Updated: 2024/05/24 09:49:10 by envillan         ###   ########.fr       */
+/*   Updated: 2024/05/24 10:37:33 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 char	*get_next_line(int fd);
 char	*ft_strdup(char *s);
 char	*ft_substr(char *s, unsigned int start, size_t len);
-char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
 void	fill_str(char *res, char *s1, char *s2);
 
