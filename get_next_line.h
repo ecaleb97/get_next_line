a3e4e9b286604854522a80cd0abfe04b05687f41
ecaleb97 <valley_rob.0c@icloud.com>
2024-06-20 18:31:26 +0200

    modified header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,25 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 19:42:43 by envillan          #+#    #+#             */
-/*   Updated: 2024/05/24 17:57:45 by envillan         ###   ########.fr       */
+/*   Updated: 2024/06/20 18:27:51 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <stddef.h>
 
 char	*get_next_line(int fd);
-char	*ft_strdup(char *s);
-char	*ft_substr(char *s, unsigned int start, size_t len);
+void	ft_bzero(void *dest, size_t n);
+size_t	ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(const char *s);
-void	fill_str(char *res, char *s1, char *s2);
+char	*ft_strchr(char *str, int c);
 
 #endif
