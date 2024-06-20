0dbd779d597441c5971b1aeafbd90a0392f59fec
ecaleb97 <valley_rob.0c@icloud.com>
2024-06-20 19:12:03 +0200

    modified bonus header file

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,25 +6,25 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 18:31:51 by envillan          #+#    #+#             */
-/*   Updated: 2024/05/24 18:36:57 by envillan         ###   ########.fr       */
+/*   Updated: 2024/06/20 19:11:11 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 15
 # endif
-# define MAX_FD 10240
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
 
 char	*get_next_line(int fd);
-char	*ft_strdup(char *s);
-char	*ft_substr(char *s, unsigned int start, size_t len);
+void	ft_bzero(void *dest, size_t n);
+size_t	ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
-void	fill_str(char *res, char *s1, char *s2);
-size_t	ft_strlen(char *s);
+char	*ft_strchr(char *str, int c);
 
 #endif
